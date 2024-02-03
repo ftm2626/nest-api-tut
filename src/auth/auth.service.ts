06b281a2cf25a42f813ade3427ba3d268bef0b21
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-02-03 14:39:02 +0330

    auth started

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signin() {
     return { msg: 'i have signed in' };
   }
 
-  signup() {
-    return { msg: 'i have signed up' };
+  async signup(dto: AuthDto) {
+    //generate password hash
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          lastName: true,
+          firstName: true,
+        },
+      });
+      //save the new user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
