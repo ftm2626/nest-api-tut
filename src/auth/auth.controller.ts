b098f6799f61b61a44f759537bfe7ae693b4f2ce
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-02-18 13:01:26 +0330

    testing setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { AuthDto } from "src/auth/dto";
+import { AuthDto } from "../../src/auth/dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post("signup")
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
+   
+  @HttpCode(HttpStatus.OK)
   @Post("signin")
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
-import { GetUser } from "src/auth/decorator";
-import { JwtGuard } from "src/auth/guard";
+import { Controller, Get, Patch, Req, UseGuards } from "@nestjs/common";
+import { User } from "@prisma/client";
+import { GetUser } from "../../src/auth/decorator";
+import { JwtGuard } from "../../src/auth/guard";
 
+@UseGuards(JwtGuard)
 @Controller("users")
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get("me")
-  getMe(@GetUser() req) {
-    return req.user;
+  getMe(@GetUser() user:User) {
+    return user;
   }
-}
+
+  @Patch()
+  editUser() {}
+} 
